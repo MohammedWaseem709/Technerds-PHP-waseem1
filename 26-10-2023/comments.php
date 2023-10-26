e50cfd635fd879866f596f1b26aad4cc1a6440df
Mohammed Waseem <mohammedwasxxm77@gmail.com>
2023-10-26 16:22:37 +0530

    26-10-2023

--- 26-10-2023/comments.php
+++ 26-10-2023/comments.php
@@ -5,6 +5,13 @@ echo $a;
 //echo $a;
 #echo $a;
 print "$a<br>";
+print "$a<br>";
+print "We<br>" .$a. "Turn code into dreams";
+echo "<br>";
+print "Welcome to"."<br>".$a;
+echo("Technerds"."<br>".$a);
+echo "<br>";
+print("Technerds"."<br>".$a);
 /*
 print "technerds"."<br>".$a;
 print "technerds"."<br>".$a;
